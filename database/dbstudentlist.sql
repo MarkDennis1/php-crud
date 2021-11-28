29ee01c69d6948bfbcbfb4b059e09b6f8878da37
Mark Dennis <markdennissanchez1@gmail.com>
2021-11-29 07:56:25 +0800

    modify renniel birthday

--- database/dbstudentlist.sql
+++ database/dbstudentlist.sql
@@ -20,7 +20,7 @@ INSERT INTO `tbl_student_profile` (`Student_ID`, `Student_First_Name`, `Student_
 INSERT INTO `tbl_student_profile` (`Student_ID`, `Student_First_Name`, `Student_Middle_Name`, `Student_Last_Name`, `Student_Suffix`, `Student_Sex`, `Student_Section`, `Student_Birthday`, `Student_Address`, `Student_Contact_Number`, `Student_Email_Address`) VALUES
 ('2', 'Mon Albert', 'Laoyon', 'Gamil', '', 'Male', 'BSIT 3-1', '2000-10-17', 'Angono, Rizal', '09693792543', 'mrgamilmonalbert@gmail.com');
 INSERT INTO `tbl_student_profile` (`Student_ID`, `Student_First_Name`, `Student_Middle_Name`, `Student_Last_Name`, `Student_Suffix`, `Student_Sex`, `Student_Section`, `Student_Birthday`, `Student_Address`, `Student_Contact_Number`, `Student_Email_Address`) VALUES
-('2', 'Renniel', 'Rocela', 'Gerongga', '', 'Male', 'BSIT 3-1', '2001-04-11', 'Angono, Rizal', '09693792543', 'itsrennieeeel@gmail.com');
+('2', 'Renniel', 'Rocela', 'Gerongga', '', 'Male', 'BSIT 3-1', '2001-04-11', 'Angono, Rizal', '09331529849', 'itsrennieeeel@gmail.com');
 INSERT INTO `tbl_student_profile` (`Student_ID`, `Student_First_Name`, `Student_Middle_Name`, `Student_Last_Name`, `Student_Suffix`, `Student_Sex`, `Student_Section`, `Student_Birthday`, `Student_Address`, `Student_Contact_Number`, `Student_Email_Address`) VALUES
 ('2', 'Jeiwel Anne', 'Pitular', 'Alcachupas', '', 'Female', 'BSIT 3-1', '2000-05-18', 'Taytay, Rizal', '09275704747', 'jeiwelanne0810@gmail.com');
 
