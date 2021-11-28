e3ae49aef660b1256b93bdffb70aaf0a3248a28c
Mark Dennis <markdennissanchez1@gmail.com>
2021-11-28 23:25:14 +0800

    create new class StudentProfile

---
+++ functions/data/studentprofile.class.php
@@ -0,0 +1,16 @@
+<?php
+
+class StudentProfile {
+
+    public $Student_ID;
+    public $Student_First_Name;
+    public $Student_Middle_Name;
+    public $Student_Last_Name;
+    public $Student_Suffix;
+    public $Student_Sex;
+    public $Student_Section;
+    public $Student_Birthday;
+    public $Student_Address;
+    public $Student_Contact_Number;
+    public $Student_Email_Address;
+}
