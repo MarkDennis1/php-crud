76e215fa33d7093380c03ff448592f7fbfdb336d
Mark Dennis <markdennissanchez1@gmail.com>
2022-01-11 01:05:36 +0800

    change config

--- config.php
+++ config.php
@@ -2,18 +2,18 @@
 
 
 //Ednis' database configuration
-// const CONFIG = [
-//     'db' => 'mysql:dbname=dbstudentlist;host=localhost',
-//     'db_user' => 'root',
-//     'db_password' => ''
-// ];
-
-
-//Mon's database configuration
 const CONFIG = [
     'db' => 'mysql:dbname=dbstudentlist;host=localhost',
     'db_user' => 'root',
-    'db_password' => 'password'
+    'db_password' => ''
 ];
+
+
+//Mon's database configuration
+// const CONFIG = [
+//     'db' => 'mysql:dbname=dbstudentlist;host=localhost',
+//     'db_user' => 'root',
+//     'db_password' => 'password'
+// ];
 
 ?>
